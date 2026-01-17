ab2339e24b46eec269c3be651a97de7cf3cf5006
berlene <berlenebernabe12@gmail.com>
2026-01-17 14:07:52 +0800

    Removed useless files

--- src/pages/admin/AdminLandingPage.jsx
+++
@@ -1,9 +0,0 @@
-export default function AdminLandingPage(){
-    return(
-        <>
-            <div className="w-svw h-svh bg-white">
-                <h1>Landing Page</h1>
-            </div>
-        </>
-    )
-}
